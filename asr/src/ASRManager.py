3faf0f39c18fcfdf731c341dbb49042d67a4d214
Raj Sikdere <sikderraj2004@.gmail.com>
2024-05-19 18:09:14 +0800

    commit1

--- asr/src/ASRManager.py
+++ asr/src/ASRManager.py
@@ -1,6 +1,6 @@
 class ASRManager:
     def __init__(self):
-        # initialize the model here
+        # initialize the model here haha
         pass
 
     def transcribe(self, audio_bytes: bytes) -> str:
